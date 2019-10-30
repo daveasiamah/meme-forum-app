be32c11f938e1a19941fcdc777fd646cc911047b
daveasiamah <daveasiamah@gmail.com>
2019-10-30 19:50:38 +0000

    fixed modal not showing by enabling bootstrap js module in index.html

--- src/views/Home.js
+++ src/views/Home.js
@@ -51,11 +51,11 @@ class Home extends Component {
           <p
             style={{
               color: "white",
-              fontSize: "2rem",
+              fontSize: "1.7rem",
               border: "1px solid #F6D114",
               borderRadius: "50%",
-              width: "65px",
-              height: "65px",
+              width: "60px",
+              height: "60px",
               backgroundColor: "#f6d114"
             }}
           >
@@ -64,7 +64,7 @@ class Home extends Component {
           <div className="form-inline">
             <button
               onClick={this.handleLogOut}
-              className="btn btn-sm btn-outline-secondary"
+              className="btn btn-sm btn-secondary"
               style={{ marginRight: "25px" }}
             >
               Logout
@@ -102,9 +102,6 @@ class Home extends Component {
               height: "38px",
               backgroundColor: "#F6D114",
               color: "white"
-            }}
-            onClick={() => {
-              console.log("Clicked Modal");
             }}
           >
             Upload Meme
@@ -208,6 +205,57 @@ class Home extends Component {
             </div>
           </div>
         </div>
+        {/* <!-- Button trigger modal --> */}
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          Launch demo modal
+        </button>
+
+        {/* <!-- Modal --> */}
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">New Modal should go here</div>
         {/* Main page */}
         <div className="container">
           <Meme />
